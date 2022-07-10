e035d74888dc1924bde5f38456ce51d115b254ff
Rohan Gupta <rohan.gupta@gameskraft.com>
2022-07-10 18:34:14 +0530

    Initial commit

---
+++ src/theme/styles.ts
@@ -0,0 +1,9 @@
+export const COLORS: { [key: string]: string } = {
+  BLACK: '#000000',
+  DARK: '#0E0F12',
+  WHITE: '#FFFFFF',
+  LIGHT: '#EDF2FA',
+  ACCENT_ONE: '#78DBE2',
+  ACCENT_TWO: '#FFF982',
+  ACCENT_THREE: '#9FFEB0',
+};
